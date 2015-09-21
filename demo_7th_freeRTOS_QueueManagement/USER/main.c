4ca138d8589d536b26f63e871887ea558cfc7dc1
grant <zhuimengcanyang@163.com>
2015-09-21 15:55:41 +0800

    example-11: send struct date

--- demo_7th_freeRTOS_QueueManagement/USER/main.c
+++ demo_7th_freeRTOS_QueueManagement/USER/main.c
@@ -11,27 +11,44 @@
 #include "portable.h"
 #include "FreeRTOSConfig.h"
 
+#define SENDER_1 (unsigned char )1
+#define SENDER_2 (unsigned char )2
+
+typedef struct data_type{
+
+    unsigned char ucValue;
+    unsigned char ucSource;
+    
+}data_type;
+
+
+static const data_type structDateToSend[2] = {
+    {100, SENDER_1},
+    {200, SENDER_2},    
+};
+
+
 /* declare a queueHandle variable, using to save queue handler. */
 xQueueHandle xQueue;
 
 
 
+
 void vSendTask(void *pvParameters)
 {
-    long valueToSend;
+    data_type * valueToSend;
     portBASE_TYPE status;
     
-    valueToSend = (long)pvParameters;
+    valueToSend = (data_type *) pvParameters;
     
     while(1)
     {
-        status = xQueueSendToBack(xQueue, &valueToSend, 0);
+        status = xQueueSendToBack(xQueue, valueToSend, 100 / portTICK_RATE_MS);
         if(status != pdPASS)
         {
             printf("could not send to the queue. \r\n");
         }
         
-        //vTaskDelay(1000 / portTICK_RATE_MS);
         taskYIELD();
     }
 }
@@ -39,20 +56,23 @@ void vSendTask(void *pvParameters)
 
 void vReceiveTask(void *pvParameters)
 {
-    long lReceivedValue;
+    data_type lReceivedValue;
     portBASE_TYPE status;
 
     while(1)
     {        
-        if( uxQueueMessagesWaiting( xQueue ) != 0 )
+        if( uxQueueMessagesWaiting( xQueue ) != 3 )
         {
-            printf( "Queue should have been empty! \r\n");
+            printf( "Queue should have been full! \r\n");
         }
 
-        status = xQueueReceive( xQueue, &lReceivedValue, 3000 / portTICK_RATE_MS );
+        status = xQueueReceive( xQueue, &lReceivedValue, 0 );
         if( status == pdPASS )
         {
-            printf( "Received = %ld\r\n", lReceivedValue );
+            if(lReceivedValue.ucSource == SENDER_1)
+                printf("from sender1 = %d \r\n", lReceivedValue.ucValue);
+            else if(lReceivedValue.ucSource == SENDER_2)
+                printf("from sender2 = %d \r\n", lReceivedValue.ucValue);
         }
         else
         {
@@ -67,17 +87,17 @@ int main(void)
     LED_Init();		  	
     uart_init(115200);
     
-    // create queue, can store 5 value which data type is long
-    xQueue = xQueueCreate(5, sizeof(long));
+    // create queue, can store 3 value which data type is data_type
+    xQueue = xQueueCreate(3, sizeof(data_type));
     
     if(xQueue != NULL)  // adjust the return value, to confirm whether create queue successful.
     {
-        // Create two write queue task, priority = 1;
-        xTaskCreate(vSendTask, "SendTask1", configMINIMAL_STACK_SIZE, (void *)100, 1, NULL);
-        xTaskCreate(vSendTask, "SendTask2", configMINIMAL_STACK_SIZE, (void *)200, 1, NULL);
+        // Create two write queue task, priority = 2;
+        xTaskCreate(vSendTask, "SendTask1", configMINIMAL_STACK_SIZE, (void *)&structDateToSend[0], 2, NULL);
+        xTaskCreate(vSendTask, "SendTask2", configMINIMAL_STACK_SIZE, (void *)&structDateToSend[1], 2, NULL);
         
-        // create one read queue task, priority = 2;
-        xTaskCreate(vReceiveTask, "RecTask", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+        // create one read queue task, priority = 1;
+        xTaskCreate(vReceiveTask, "RecTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
         
         // start scheduler now
         vTaskStartScheduler();            
